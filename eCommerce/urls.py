4e135c2e0bb74411ab88022e41878fee419e20a5
kanga sylvestre <kangasylvestre28@gmail.com>
2022-05-07 20:01:27 +0100

    sauvegarde apres configuration des fichiers html et d'une parti des fichiers static

--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls', namespace='shop'))
+    path('', include('shop.urls', namespace='shop')),
+    path('', include('admin_shop.urls', namespace='admin_shop'))
+
 ]

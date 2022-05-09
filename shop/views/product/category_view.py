213cebec63850165167d49ae66e07c630d89ecc5
kbjeanelie <jeaneliekubembula@gmail.com>
2022-05-10 00:59:35 +0100

    - LISTEs LES PRODUITS SELON LEUR CATEGORY

--- shop/views/product/category_view.py
+++ shop/views/product/category_view.py
@@ -1,16 +1,13 @@
-from django.views import View
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView
 
-from admin_shop.models import Product
+from admin_shop.models import Product, Category
 
 
-class CategoryListProductView(ListView):
-    context_object_name = 'products'
-    queryset = Product.objects.all().order_by('prices')
+class CategoryListProductView(DetailView):
+    model = Category
     template_name = 'shop/product/shop.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.filter(category=self.object).order_by('prices')
         return context
-
